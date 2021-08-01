c13994776a200dd358e4ae3ea8e1aac3f9eeeb78
deepak-webdev <deepakwebdev12@gmail.com>
2021-08-01 23:01:09 +0530

    integrated firebase into application, created sign in page with sign components and sign in with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyApULy3lVEIu6TA7Fwe2Zh7QeynuQbkM6M",
+  authDomain: "crwn-db-17dad.firebaseapp.com",
+  projectId: "crwn-db-17dad",
+  storageBucket: "crwn-db-17dad.appspot.com",
+  messagingSenderId: "50049765606",
+  appId: "1:50049765606:web:c07fac9156dba6f6ed6b27",
+  measurementId: "G-FC4GMNBX47",
 };
 
 firebase.initializeApp(config);
@@ -18,7 +18,8 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
+
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
